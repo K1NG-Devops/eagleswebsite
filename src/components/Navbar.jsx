ee9972efd129e1c1c5d649a599f8ae0157f65eec
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-04-18 16:34:21 +0200

    Changed background color of Navbar and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="navbar rounded-box shadow-base-300/20 shadow-sm bg-gray-900 sm:hidden" aria-label="Main Navigation">
-    <div className="w-full md:flex md:items-center md:gap-2">
-      <div className="flex items-center justify-between">
-        <Link to="/" className="text-2xl font-bold text-blue-800">Young Eagles</Link>
-        <ul className="flex items-center justify-between space-x-4">
-          <li><Link to="/" className="hover:text-blue-600">Home</Link></li>
-          <li><Link to="/about" className="hover:text-blue-600">About Us</Link></li>
-          <li><Link to="/admission" className="hover:text-blue-600">Admission</Link></li>
-          <li><Link to="/programs" className="hover:text-blue-600">Our Programs</Link></li>
-          <li><Link to="/contact" className="hover:text-blue-600">Contact</Link></li>
-        </ul>
+  <nav className="navbar text-gray-900 py-1 rounded-xl shadow-md mt-2">
+    <div className="flex justify-between items-center w-full mx-auto">
+      <div className='flex items-center'>
+      <Link to="/" className="logo">Youngeagles</Link>
       </div>
+      
+      <ul className="nav-menu flex flex-row text-lg">
+        <li><Link to="/" className="hover:text-blue-900">Home</Link></li>
+        <li><Link to="/about" className="hover:text-blue-300">About Us</Link></li>
+        <li><Link to="/admission" className="hover:text-blue-300">Admission</Link></li>
+        <li><Link to="/programs" className="hover:text-blue-300">Our Programs</Link></li>
+        <li><Link to="/contact" className="hover:text-blue-300">Contact</Link></li>
+      </ul>
     </div>
   </nav>
 );
